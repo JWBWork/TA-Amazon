c8d5132128b2d1339f41d42abf62f90c6070b022
Jacob Brooks <JacobBrooksWork@gmail.com>
2021-01-28 11:14:50 -0500

    rename main to tasks for robot framework

--- tasks.py
+++ tasks.py
@@ -5,14 +5,12 @@ from src.emailresults import send_results
 def main():
     try:
         results = search_shopping_list()
+        cheapest = dict()
+        for search_term, results in results.items():
+            cheapest[search_term] = min(results, key=lambda r: r['price'])
+        send_results(cheapest)
     finally:
         close_driver()
-
-    cheapest = dict()
-    for search_term, results in results.items():
-        cheapest[search_term] = min(results, key=lambda r: r['price'])
-
-    send_results(cheapest)
 
 
 if __name__ == '__main__':
